571b57fb738500724a4344d3f98257f913cb894b
Abdulrahman <tranegame@gmail.com>
2021-05-21 14:17:55 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [file, setFile] = useState();
+  const [description, setDescription] = useState("");
+  const [uploadedFile, setUploadedFile] = useState();
+  const [filesName, setFileName] = useState("");
+
+  const handleChange = (event) => {
+    setFile(event.target.files[0]);
+    setFileName(event.target.files[0].name);
+  };
+
+  const handleForm = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const res = await axios.post("http://localhost:3001/upload", formData);
+      const { fileName, filePath } = res.data;
+      setUploadedFile({ fileName, filePath });
+      console.log(uploadedFile.filePath);
+    } catch (err) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ms">
+      <main>
+        <form onSubmit={handleForm} className="formS">
+          <div className="ac">
+            <label htmlFor="text">Description</label>
+            <input
+              type="text"
+              name="description"
+              value={description}
+              onChange={(event) => {
+                setDescription(event.target.value);
+              }}
+            />
+
+            <input type="file" name="file" onChange={handleChange} />
+
+            <button type="submit">Upload</button>
+          </div>
+        </form>
+
+        <div className="disImg">
+          {uploadedFile && (
+            <>
+              <img src={filesName} alt="123" />
+              <p>{description}</p>
+            </>
+          )}
+        </div>
+      </main>
     </div>
   );
 }
